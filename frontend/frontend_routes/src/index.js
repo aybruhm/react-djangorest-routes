d6e6587a026b34649613c33786edbbb240d10804
Israel Abraham <israelvictory87@gmail.com>
2022-04-02 10:14:08 +0100

    Upgrade dom client

--- frontend/frontend_routes/src/index.js
+++ frontend/frontend_routes/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import * as ReactDOMClient from 'react-dom/client';
+
+const app = document.getElementById('root');
+
+// Create a root 
+const root = ReactDOMClient.createRoot(app);
+
+// Initial render: Render an element to the root.
+root.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+)
