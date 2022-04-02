69d6a5ba9ddac3f70cf74392830e6af66965b116
Israel Abraham <israelvictory87@gmail.com>
2022-04-02 09:27:03 +0100

    Added Card compoenent

--- frontend/frontend_routes/src/App.js
+++ frontend/frontend_routes/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/layouts/Navbar';
+import Card from './components/layouts/Card';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      
+      <Card/>
     </div>
   );
 }
