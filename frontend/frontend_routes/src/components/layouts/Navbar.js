eb45de6e05830583e4fcf8bfa5c9abdadae83ded
Israel Abraham <israelvictory87@gmail.com>
2022-04-02 09:26:43 +0100

    Added auth-routes -> register, login

--- frontend/frontend_routes/src/components/layouts/Navbar.js
+++ frontend/frontend_routes/src/components/layouts/Navbar.js
@@ -4,9 +4,18 @@ import React from 'react';
 function Navbar() {
     return (
         <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
+            <div class="container">
                 <span class="navbar-brand mb-0 h1">react-djangorest-routes</span>
             </div>
+
+
+            <a href="#" class="btn btn-link px-3 me-2">
+                register
+            </a>
+
+            <a href="#" class="btn btn-link px-3 me-2">
+                Login
+            </a>
         </nav>
     )
 }
