d9c8c16b23fc71c60022535ced81362cb4a10252
Israel Abraham <israelvictory87@gmail.com>
2022-04-02 10:14:21 +0100

    Refactor class to className

--- frontend/frontend_routes/src/components/layouts/Navbar.js
+++ frontend/frontend_routes/src/components/layouts/Navbar.js
@@ -3,19 +3,43 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
-                <span class="navbar-brand mb-0 h1">react-djangorest-routes</span>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                <span className="navbar-brand mb-0 h1">react-djangorest-routes</span>
             </div>
 
 
-            <a href="#" class="btn btn-link px-3 me-2">
-                register
-            </a>
+            <div className="auth-lists">
+                <a href="#" className="btn btn-link px-3 me-2">
+                    register
+                </a>
 
-            <a href="#" class="btn btn-link px-3 me-2">
-                Login
-            </a>
+                <a href="#" className="btn btn-link px-3 me-2">
+                    Login
+                </a>
+            </div>
+
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#auth-lists-mobile-content"
+                aria-controls="auth-lists-mobile-content"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <i className="fas fa-bars"></i>
+            </button>
+
+            <div className="auth-lists-mobile" id="auth-lists-mobile-content">
+                <a href="#" className="btn btn-link px-3 me-2">
+                    register
+                </a>
+
+                <a href="#" className="btn btn-link px-3 me-2">
+                    Login
+                </a>
+            </div>
         </nav>
     )
 }
