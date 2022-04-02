0589f70ed11899f19b7beab6f60d516875a368e8
Israel Abraham <israelvictory87@gmail.com>
2022-04-02 08:54:04 +0100

    Created Navbar and Card components

---
+++ frontend/frontend_routes/src/components/layouts/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">react-djangorest-routes</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
