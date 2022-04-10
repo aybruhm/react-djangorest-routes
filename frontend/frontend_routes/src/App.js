7ed7d8d25106ae17291d169b8a40a19ec5f974ee
Israel Abraham <israelvictory87@gmail.com>
2022-04-10 07:31:33 +0100

    Integrate react-router-dom

--- frontend/frontend_routes/src/App.js
+++ frontend/frontend_routes/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Card from './components/layouts/Card';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      
-      <Card/>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+
+            <Routes>
+              <Route path="/" exact element={ <Card /> }></Route>
+
+              <Route path="/login/" component={ <Login /> } element={ <Login /> }></Route>
+
+              <Route path="/register/" component={ <Register /> } element={ <Register /> }></Route>
+            </Routes>
+            
+        </div>
+    </BrowserRouter>
   );
 }
 
